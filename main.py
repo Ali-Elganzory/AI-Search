5d87106692f494dea18b42338357f0dd5dac6385
Ali Elganzory <alielganzory@hotmail.com>
2021-04-23 15:10:01 +0200

    progressed the GUI + added favicon + added help modal

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def window_updated():
 
 
 def update(event=None):
+	# Clear the canvas
 	ctx.clearRect(0, 0, width, height)
-
 	ctx.save()
 
+	# Draw the grid
 	ctx.strokeStyle = "purple"
 	ctx.beginPath()
 	for i in range(grid_dimensions[0]):
@@ -34,14 +35,17 @@ def update(event=None):
 
 
 def mousemove(event):
+	# Return in case the event is out of the grid coordinates
 	if event.x < left_padding or event.x > left_padding + (grid_dimensions[1] * cell_size) \
 		or event.y < top_padding or event.y > top_padding + (grid_dimensions[0] * cell_size):
 		return
-		
+
+	# Modify the grid with the appropriate tool
 	if event.button == 0:
 		grid[(event.y-top_padding)//cell_size][(event.x-left_padding)//cell_size] = selected_tool
 
 
+# Setter for the [selected_tool]
 def select_tool(tool):
 	global selected_tool
 	selected_tool = tool
@@ -50,21 +54,24 @@ def select_tool(tool):
 
 ####	Main		####
 
+# Constants
+grid_dimensions = (10, 10)
+grid = [["empty" for j in range(grid_dimensions[0])] for i in range(grid_dimensions[1])]
+cell_size = 40
+top_padding = 1 * 38 + 2 * 24
+
 # Getting and setting canvas
 canvas = document["canvas"]
 ctx = canvas.getContext("2d")
 width = window.innerWidth
 height = window.innerHeight
 canvas["width"] = width
-top_padding = 1 * 38 + 2 * 27
-canvas["height"] = height - top_padding
+canvas["height"] = grid_dimensions[0] * cell_size
 
-# Constants
-grid_dimensions = (10, 10)
-grid = [["empty" for j in range(grid_dimensions[0])] for i in range(grid_dimensions[1])]
-cell_size = 40
+# Rest of constants that incorporate the canvas dimensions in calculation
 left_padding = (width - (grid_dimensions[0] * cell_size)) // 2
 
+# Mapping tools to their respective colors
 selected_tool = "blocked"
 colors = { 
 	"blocked": "gray",
@@ -73,7 +80,7 @@ colors = {
 	"target": "green"
 }
 
-# Mouse event listeners
+# Binding listeners to tools and mousedown events
 document["block"].bind("click", lambda e: select_tool("blocked"))
 document["empty"].bind("click", lambda e: select_tool("empty"))
 document["source"].bind("click", lambda e: select_tool("source"))
